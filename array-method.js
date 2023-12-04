c6d30b603295e244dbdf1435078b1345724c4aee
Arunkum20072002 <arunkumarmarikani10@gmail.com>
2023-12-04 10:53:18 +0530

    Array-method-1

---
+++ array-method.js
@@ -0,0 +1,28 @@
+document.write("1.Make a given array empty"+"<br>")
+document.write("--------------------------"+"<br>")
+
+var fruits = ['apple', 'orange', 'banana', 'grapes'];
+console.log(fruits)
+
+ var fruits=[];
+console.log(fruits)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
